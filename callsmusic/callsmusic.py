f2d961943f645fe76be67b86b1192abb55880528
Lankesh Ravan <lankeshravan67@gmail.com>
2021-03-28 16:47:33 +0530

    everythingFIXED

--- callsmusic/callsmusic.py
+++ callsmusic/callsmusic.py
@@ -11,10 +11,6 @@ pytgcalls = PyTgCalls(client)
 @pytgcalls.on_stream_end()
 def on_stream_end(chat_id: int) -> None:
     queues.task_done(chat_id)
-
-    if queues.is_empty(chat_id):
-        await message.reply_text(f"**{BN} :-** Abe Gaane DO be kaunse bajau Khatam HO gaye Queue mai")
-    else:
         pytgcalls.change_stream(
             chat_id, queues.get(chat_id)["file_path"]
         )
