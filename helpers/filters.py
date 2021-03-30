cd9c75974fc4a4163e293fd9f846062a24bb7119
Lankesh Ravan <lankeshravan67@gmail.com>
2021-03-30 10:15:53 +0530

    SpamOP

--- helpers/filters.py
+++ helpers/filters.py
@@ -4,7 +4,7 @@ from pyrogram import filters
 
 from config import COMMAND_PREFIXES
 
-other_filters = filters.group & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
+other_filters = filters.group & ~ filters.edited & ~ filters.forwarded
 other_filters2 = filters.private & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
 
 
